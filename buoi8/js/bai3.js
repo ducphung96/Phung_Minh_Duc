1acac228b272ddc5f061075999a3e0469357ce57
ducphung96 <phungminhduc18111996@gmail.com>
2018-11-04 21:26:33 +0700

    nop bai tap buoi 8

--- buoi8/js/bai3.js
+++ buoi8/js/bai3.js
@@ -1,13 +1,15 @@
 function bai3(n){
 	var n = parseInt(prompt("Mời bạn nhập n : "));
-	document.write("Tổng s là: " + bai3(n));
-	function bai3(n){
-		var s = 0;
-		var i = 1;
-		while(i<=n){
-			s += 1/i;
-			i++;
-		}
-		return s;
-	}
+	var s = 0;
+	if (n <= 0)
+	{
+       alert("Bạn phải nhập số > 0");
+    }
+    else
+    {
+        for (var i = 1; i <= n; i++){
+            s += 1/i;
+        }
+    document.write("Tổng giá trị s là: " + s);
+    }
 }
