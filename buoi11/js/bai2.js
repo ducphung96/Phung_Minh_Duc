ef523aba38747f59999a3424acd27c241852bd9c
ducphung96 <phungminhduc18111996@gmail.com>
2018-11-15 23:00:05 +0700

    nop bai tap buoi 11

--- buoi11/js/bai2.js
+++ buoi11/js/bai2.js
@@ -7,12 +7,18 @@ $(function(){
 	if (index.parent().next().css('display') == 'none') {
 		btn.html('<i class="fas fa-plus"></i>')
 		ct.css('display','none')
+		txt.css('display','block')
+		txt.next().css('display','none')
 		txt.css('color','#fff')
 		index.parent().next().css('display', 'block')
 		index.html('<i class="fas fa-minus"></i>')
-		index.next().css('color', '#CCFF33')
+		index.next().next().css('color', '#CCFF33')
+		index.next().next().css('display', 'block')
+		index.next().css('display', 'none')
 		
 	}else{
+		index.next().css('display', 'block')
+		index.next().next().css('display', 'none')
 		index.parent().next().css('display', 'none')
 		index.html('<i class="fas fa-plus"></i>')
 		index.next().css('color', '#fff')
