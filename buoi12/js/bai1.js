513b2a8a6aa77c43c8f5789e1bc91849441a1a05
ducphung96 <phungminhduc18111996@gmail.com>
2018-11-19 23:29:30 +0700

    nop bai tap buoi 12

--- buoi12/js/bai1.js
+++ buoi12/js/bai1.js
@@ -2,16 +2,28 @@ $(document).ready(function(){
     var index = $('#form');
     $('#next').on('click',function(){
         if(index.children().eq(3).css('display') == 'block') {
-            if ($('#checkbox').is(':checked')) {
+            $('#item4').on('click', function(){
+                $('#finish').css('display', 'block');
+                $('#account,#profile,#warning').css('display', 'none');
+                $(this).css('background', '#2184be');
+                $('#item1,#item2,#item3').css('background', '#9dc8e2');
+            });
+            if ($('#checkbox').prop('checked')) {
                 $('#item4').css('background','#2184be');
-                $('#errorcheck').text('');
+                $('#errorcheck').html('');
                 alert('Chúc mừng bạn đã hoàn thành');
             }else{
                 $('#item4').css('background','red');
-                $('#errorcheck').text('This field is required.');
+                $('#errorcheck').html('Vui lòng tích vào.');
             }
         };  
         if(index.children().eq(2).css('display') == 'block') {
+            $('#item3').on('click', function(){
+                $('#warning').css('display', 'block');
+                $('#account,#profile,#finish').css('display', 'none');
+                $(this).css('background', '#2184be');
+                $('#item1,#item2,#item4').css('background', '#9dc8e2');
+            });
             var i = true;
             var age = $('#age').val();
             if (parseInt(age)<18) {
@@ -26,46 +38,55 @@ $(document).ready(function(){
             }
         }; 
         if(index.children().eq(1).css('display') == 'block') {
-            var validateemail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            function validateEmail(email) {
+                var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return re.test(String(email).toLowerCase());
+            }
             var email = $('#email').val();
             var firstname = $('#firstname').val();
             var lastname = $('#lastname').val();
             var addre = $('#addre').val();
             var age = $('#age').val();
             var i = true;
+            $('#item2').on('click', function(){
+                $('#profile').css('display', 'block');
+                $('#account,#warning,#finish').css('display', 'none');
+                $(this).css('background', '#2184be');
+                $('#item1,#item3,#item4').css('background', '#9dc8e2');
+            });
             if(firstname == '') {
-                $('#errorfirstname').text('This field is required.');
+                $('#errorfirstname').html('Tên Không được để trống.');
                 i = false;
             }else{
-                $('#errorfirstname').text('');
+                $('#errorfirstname').html('');
             }
             if(lastname == '') {
-                $('#errorlastname').text('This field is required.');
+                $('#errorlastname').html('Họ không được để trống.');
                 i = false;
             }else{
-                $('#errorlastname').text('');
+                $('#errorlastname').html('');
             }
             if(email == '') {
-                $('#erroremail').text('This field is required.');
+                $('#erroremail').html('Email không được để trống.');
                 i = false;
-            }else if(!validateemail.test(email)) {
-                $('#erroremail').text('Please enter a valid email address.');
+            }else if(!validateEmail(email)) {
+                $('#erroremail').html('Đinh dạng email abc@gmail.com!');
                 i = false;
             }else{
-                $('#erroremail').text('');
+                $('#erroremail').html('');
             }
             if(addre == '') {
-                $('#erroraddre').text('This field is required.');
+                $('#erroraddre').html('Địa chỉ không được để trống.');
                 i = false;
             }else{
-                $('#erroraddre').text('');
+                $('#erroraddre').html('');
             }
             if(age == '') {
-                $('#errorage').text('This field is required.');
+                $('#errorage').html('Mời bạn nhập tuổi.');
                 i = false;
             }
             else{
-                $('#errorage').text('');
+                $('#errorage').html('');
             }
             if(i) {
                 index.children().eq(1).css('display','none');
@@ -73,42 +94,48 @@ $(document).ready(function(){
                 $('#item2').css('background','#9dc8e2');
                 $('#item3').css({'background':'#2184be','color':'#fff'});
                 if (parseInt(age)<18) {
-                    $('#errorwarn').text('Must be over 18 years old! - Go back to the previous step!')
+                    $('#errorwarn').html('Bạn chưa đủ 18 tuổi! - Mời bạn quay lại!')
                     $('#item3').css('background','red')
                 } else{
-                    $('#errorwarn').text('No warning.')
+                    $('#errorwarn').html('Bạn đã đủ điều kiện - Đi tiếp.')
                 }
             }else{
                 $('#item2').css('background','red');
             }
         };
         if(index.children().eq(0).css('display') == 'block') {
+            $('#item1').on('click', function(){
+                $('#account').css('display', 'block');
+                $('#profile,#warning,#finish').css('display', 'none');
+                $(this).css('background', '#2184be');
+                $('#item2,#item3,#item4').css('background', '#9dc8e2');
+            });
             var name = $('#name').val()
             var password = $('#password').val();
             var repassword = $('#repassword').val();
             var i = true;
             if(name == '') {
-                $('#errorname').text('This field is required.');
+                $('#errorname').html('Tên không được để trống.');
                 i = false;
             }
             else{
-                $('#errorname').text('');
+                $('#errorname').html('');
             }
             if(password == '') {
-                $('#errorpassword').text('This field is required.');
+                $('#errorpassword').html('Password không được để trống.');
                 i = false;
             }
             else{
-                $('#errorpassword').text('');
+                $('#errorpassword').html('');
             }
             if(repassword == '') {
-                $('#error-repassword').text('This field is required.');
+                $('#error-repassword').html('Confirm Password không được để trống.');
                 i = false;
             }else if(password != repassword) {
-                $('#error-repassword').text('confirm password is not correct.');
+                $('#error-repassword').html('Confirm Password phải giống password ở trên.');
                 i = false;
             }else{
-                $('#error-repassword').text('');
+                $('#error-repassword').html('');
             }
             if(i) {
                 index.children().eq(0).css('display','none');
@@ -122,11 +149,12 @@ $(document).ready(function(){
         }; 
     });
     $('#prev').on('click',function(){
-        var prev = $('.index')
+        var prev = $('.index');
         for (var i = 1; i < prev.length; i++) {
             if (prev.eq(i).css('display') == 'block') {
                 prev.eq(i).css('display','none');
                 prev.eq(i-1).css('display','block');
+                $('#next').val('next');
                 $('#item'+i).css('background','#2184be');
                 $('#item'+(i+1)).css('background','#9dc8e2');
             };
